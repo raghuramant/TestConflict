98574d81762868c2b0c617d8c857b410b7d5f6b5
raghuramant <raghuramanth@gmail.com>
2018-06-15 09:06:33 +0530

    user2 task

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -5,6 +5,10 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
+		System.out.println("user2 chenages");
 	}
 
+	void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
